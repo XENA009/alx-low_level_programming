90855fb1a7d6b0bc641ac9386e7665b97e4d6f4a
XENA009 <jihaneeljid@gmail.com>
2023-03-16 17:47:02 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int main(void){
+
+/**
+ * main - Entry point
+ *
+ * Description: a C program that prints with puts function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
     puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
