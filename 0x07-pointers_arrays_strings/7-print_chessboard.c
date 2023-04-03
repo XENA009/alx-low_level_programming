5b18fa475686037b567b537100ad0c5ae4c084c9
XENA009 <jihaneeljid@gmail.com>
2023-04-03 16:08:35 -0700

    a

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,6 +1,12 @@
 #include "main.h"
 
-
+/**
+ * _strpbrk - Search a string for any of a set of bytes.
+ * @s: string
+ * @accept: string to match
+ * Return: Pointer to the byte in s that matches one of the bytes in accept
+ * or NULL if no such byte is found.
+ */
 
 
 void print_chessboard(char (*a)[8])
