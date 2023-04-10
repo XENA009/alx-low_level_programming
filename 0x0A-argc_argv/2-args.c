0fa457a0f2db8ca0cad94adf70eedc781829a23f
XENA009 <jihaneeljid@gmail.com>
2023-04-09 17:26:09 -0700

    a

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the number of the program's passed arguments 
+ * main - prints the number of the program's passed arguments
  * @argc : int
  * @argv : list
  * Return : 0
