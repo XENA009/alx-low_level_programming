3c6fda31cf27da285a0a59d1ca70c080ac4bf537
XENA009 <jihaneeljid@gmail.com>
2023-04-09 17:24:31 -0700

    almost forgot number 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of the program's passed arguments 
+ * @argc : int
+ * @argv : list
+ * Return : 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	int i = 0;
+
+	while (argc--)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+
+	return (0);
+}
